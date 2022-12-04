ac66abf643298a4dc9fd03e9226657c8428025d4
Aketzalli <50631849+Zalligar@users.noreply.github.com>
2022-12-04 16:58:42 -0600

    Updated

---
+++ Fire.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+func _ready():
+	$Tween.interpolate_property($Fires, 'rect_scale', Vector2(0.5,0.5), Vector2(1.25,1.25), 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$Tween.interpolate_property($Fires, 'modulate:a', \
+		1,0,0.35,Tween.TRANS_QUAD,Tween.EASE_OUT)
+	#pass # Replace with function body.
+	$Tween.start()
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Tween_tween_all_completed():
+	queue_free()
+	pass # Replace with function body.
